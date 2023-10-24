535ad277d80e43c5def6655b9953722b585ef11b
MATÍAS ABOUDARA <47701750@ort>
2023-10-24 08:34:48 -0300

    Creación del proyecto + gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP11_ProyectoIntegral.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
