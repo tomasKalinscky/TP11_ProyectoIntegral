82e1a2e3b8fdc643f44395cdf881e9341ab6c489
MAXIMILIANO GARBARINO <47433817@ort>
2023-10-31 08:46:20 -0300

    Avanzando con BD y un poco del index

--- Models/Libro.cs
+++ Models/Libro.cs
@@ -1,14 +1,14 @@
 namespace TP11_ProyectoIntegral.Models;
-public class Libros{
+public class Libro{
     public int IDLibro {get; set;}
     public int FKAutor {get; set;}
     public int FKGenero {get; set;}
     public date FechaDePublicacion {get; set;}
     public string Descripcion {get; set;}
-    public Libros(){
+    public Libro(){
 
     }
-    public Libros(int IDLibro = 0,int FKAutor = 0,int FKGenero =0,date FechaDePublicacion = null,string Descripcion = ""){
+    public Libro(int IDLibro = 0,int FKAutor = 0,int FKGenero =0,date FechaDePublicacion = null,string Descripcion = ""){
         this.IDLibro = IDLibro;
         this.FKAutor = FKAutor;
         this.FKGenero = FKGenero;
