96e0174b7290e3028e84a5c1b6cc64f56b01359d
MAXIMILIANO GARBARINO <47433817@ort>
2023-11-07 08:41:17 -0300

    avance del layout y de las views

--- Models/Libro.cs
+++ Models/Libro.cs
@@ -5,10 +5,11 @@ public class Libro{
     public int FKGenero {get; set;}
     public DateOnly FechaDePublicacion {get; set;}
     public string Descripcion {get; set;}
+    public int Stock {get; set;}
     public Libro(){
 
     }
-    public Libro(int IDLibro = 0,int FKAutor = 0,int FKGenero =0,DateOnly FechaDePublicacion = new DateOnly() ,string Descripcion = ""){
+    public Libro(int IDLibro = 0,int FKAutor = 0,int FKGenero =0,DateOnly FechaDePublicacion = new DateOnly() ,string Descripcion = "", int Stock = 0){
         this.IDLibro = IDLibro;
         this.FKAutor = FKAutor;
         this.FKGenero = FKGenero;
