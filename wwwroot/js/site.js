8666f20713f2e3f1220bb0449ccf3a123fd3a05b
MAXIMILIANO GARBARINO <47433817@ort>
2023-11-30 15:08:23 -0300

    Raian lo hizo otra vez

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -92,3 +92,26 @@ function MostrarHistorialCompras(IDS)
     <input type="radio" name="FKMetodoDePago">MercadoPago QR
     <input type="radio" name="FKMetodoDePago">MercadoPago Transferencia
   </form>*/ 
+  function validarFormulario() {
+    let password = document.getElementById("Contrasena").value;
+    let confirmPassword = document.getElementById("Contrasena2").value;
+  
+    var caracterEspecial = /[$&+,:;=?@#|'<>.^*()%!-]/.test(password);
+  
+    /*if (!caracterEspecial) {
+        document.getElementById("mensajeError").innerHTML = "La contraseña debe contener al menos un carácter especial.";
+        return false;
+    } */
+  
+    if (password.length < 8) {
+        document.getElementById("mensajeError").innerHTML = "La contraseña debe tener al menos 8 caracteres.";
+        return false;
+    }
+  
+    if (confirmPassword !== password) {
+        document.getElementById("mensajeError").innerHTML = "Las contraseñas no coinciden. Por favor, inténtelo de nuevo.";
+        document.getElementById("Contrasena2").value = "";
+        return false;
+    }
+    return true;
+  }
